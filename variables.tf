d0c201e590cc14f28af0a78292f21822a2871680
the_beginning <97131744+0xthebeginning@users.noreply.github.com>
2025-05-21 15:12:54 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region to use"
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  default     = "my-terraform-mock-bucket"
+}
